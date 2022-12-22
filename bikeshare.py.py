238c69586680725829fd42180398aef216015f5f
Sadeem Abdullah Alharthi <sadim3bdullah@gmail.com>
2022-12-22 04:07:46 +0300

    refactor: added new print statement showing at the end of the code

--- bikeshare.py.py
+++ bikeshare.py.py
@@ -187,7 +187,7 @@ def main():
         restart = input('\nWould you like to restart? Enter yes or no.\n')
         if restart.lower() != 'yes':
             break
-
+    print("I hope your exploration on US bikeshare data was fun and helpful.")
 
 if __name__ == "__main__":
 	main()
